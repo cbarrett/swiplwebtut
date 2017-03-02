3515fcb8a157c8b388dcb745b20f648161579596
Colin Barrett <colin@springsandstruts.com>
2017-03-02 01:44:17 -0500

    2.2 Exercise (harder): serve up a small png image just using format calls

--- helloweb.pl
+++ helloweb.pl
@@ -11,8 +11,7 @@ server(Port) :-
 % Here our path is / (the root) and the goal we'll query will be
 % say_hi. The third argument is for options
 :- http_handler(/, say_hi, []).
-:- http_handler('/friends', hello(friends), []).
-:- http_handler('/ladies', hello(ladies), []).
+:- http_handler('/image.png', image, []).
 
 /* The implementation of /. The single argument provides the request
 details, which we ignore for now. Our task is to write a CGI-Document:
@@ -26,10 +25,16 @@ say_hi(_Request) :-
         format('Content-type: text/plain~n~n'),
         format('Hello World!~n').
 
-hello(ladies, _Request) :-
-        format('Content-type: text/plain~n~n'),
-        format('Hellooooooo ladies!!~n').
-    
-hello(friends, _Request) :-
-        format('Content-type: text/plain~n~n'),
-        format('Hello, friends.~n').
+image(_Request) :-
+    format('Content-type: image/png~n~n'),
+    format('\x89\\x50\\x4e\\x47\\x0d\\x0a\\x1a\\x0a\\x00\\x00\\x00\\x0d\\x49\\x48\\x44\\x52\'),
+    format('\x00\\x00\\x00\\x20\\x00\\x00\\x00\\x20\\x01\\x00\\x00\\x00\\x00\\x5b\\x01\\x47\'),
+    format('\x59\\x00\\x00\\x00\\x04\\x67\\x41\\x4d\\x41\\x00\\x01\\x86\\xa0\\x31\\xe8\\x96\'),
+    format('\x5f\\x00\\x00\\x00\\x5b\\x49\\x44\\x41\\x54\\x78\\x9c\\x2d\\xcc\\xb1\\x09\\x03\'),
+    format('\x30\\x0c\\x05\\xd1\\xeb\\xd2\\x04\\xb2\\x4a\\x20\\x0b\\x7a\\x34\\x6f\\x90\\x15\'),
+    format('\x3c\\x82\\xc1\\x8d\\x0a\\x61\\x45\\x07\\x51\\xf1\\xe0\\x8a\\x2f\\xaa\\xea\\xd2\'),
+    format('\xa4\\x84\\x6c\\xce\\xa9\\x25\\x53\\x06\\xe7\\x53\\x34\\x57\\x12\\xe2\\x11\\xb2\'),
+    format('\x21\\xbf\\x4b\\x26\\x3d\\x1b\\x42\\x73\\x25\\x25\\x5e\\x8b\\xda\\xb2\\x9e\\x6f\'),
+    format('\x6a\\xca\\x30\\x69\\x2e\\x9d\\x29\\x61\\x6e\\xe9\\x6f\\x30\\x65\\xf0\\xbf\\x1f\'),
+    format('\x10\\x87\\x49\\x2f\\xd0\\x2f\\x14\\xc9\\x00\\x00\\x00\\x00\\x49\\x45\\x4e\\x44\'),
+    format('\xae\\x42\\x60\\x82\').
